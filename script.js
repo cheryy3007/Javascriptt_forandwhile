ce4612f375c81719f91de5a4b6ad1606eb4541ca
cheryy3007 <zulfiyagazna@gmail.com>
2024-11-25 18:11:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let array = [2, 4, 9, 10, 45, 67, 8, 90]; 
+
+let evenNumbers = array.filter(num => num % 2 === 0);
+
+console.log(array, "начала array"); 
+console.log(evenNumbers, "чётные числа array"); 
+
+
+
